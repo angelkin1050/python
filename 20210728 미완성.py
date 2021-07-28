7732ffff96f7db1575df4a135305501ad70e1330
angelkin1050 <87740745+angelkin1050@users.noreply.github.com>
2021-07-28 12:21:01 +0900

    20210728 미완성

---
+++ 20210728 미완성.py
@@ -0,0 +1,10 @@
+"""
+print("b mi계산기 입니다.")
+weight = float(input("몸무게를 입력해주세요"))
+height = float(input("키를 입력해주세요"))
+print("측정도 : weight / %f*%f = %f"%(weight,height,height,))
+"""
+score = int(input("점수를 입력하세요"))
+if score>=90:
+    print("수")
+    
