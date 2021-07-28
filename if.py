67e9bad88adb9c9668e5783fc524c7e175580880
angelkin1050 <87740745+angelkin1050@users.noreply.github.com>
2021-07-28 12:01:36 +0900

    20210728 if

---
+++ if.py
@@ -0,0 +1,27 @@
+"""
+if : 만약 ~ 라면
+IF조건 :
+ code1
+ code2
+ == 같다
+ !  같지않다
+ < <= > >=
+ True,False
+number = 4
+if number ==
+4:
+ print("true")
+ print("true2")
+ 
+number = int(input("숫자를 입력해주세요"))
+if number>10:
+  print("10보다 큰 수를 입력하셨습니다")
+"""
+number = int(input("숫자를 입력해주세요"))
+if number<0:
+ print("음수")
+elif number == 0:
+ print("0")
+else :
+ print("양수")  
+
