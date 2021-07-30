d507294be23a5d08b3e5e798d7a3560826464e1b
angelkin1050 <87740745+angelkin1050@users.noreply.github.com>
2021-07-30 12:19:51 +0900

    20210730 세개 수 비교하기 미완성

---
+++ 세개 수 비교하기.py
@@ -0,0 +1,11 @@
+num1 = int(input("첫번 째 정수를 입력하시오"))
+num2 = int(input("두번 째 정수를 입력하시오"))
+num3 = int(input("세번 째 정수를 입력하시오"))
+if (num1<num2):
+   max = num2
+else:
+   max = num1
+if (max<num3):
+    max = num3
+else:
+    max = max
