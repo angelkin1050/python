5bb25a9d3604da9f463765edbff67723e819c452
angelkin1050 <87740745+angelkin1050@users.noreply.github.com>
2021-08-06 11:33:23 +0900

    20210806 while

---
+++ while.py
@@ -0,0 +1,19 @@
+
+반목문 while(~하는 동안에)
+조건이 참일동안 코드 반복
+
+자연수 10부터 30까지 차례대로 출력하는 프로그램
+
+startnum = 10
+while startnum <= 20:
+    print(startnum)
+    startnum = startnum + 1
+    
+print("마지막 값 : %d"%startnum)
+
+startnum = 9
+while startnum <= 19:
+    startnum = startnum + 1
+    print(startnum)
+    
+print("마지막 값 : %d"%startnum)
